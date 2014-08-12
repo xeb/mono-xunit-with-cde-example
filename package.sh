23f685ca30c57c4f1c8cf09f64b8b0f8348bc445
Mark Kockerbeck <mark@kockerbeck.com>
2014-08-12 04:24:42 -0400

    moving implementation into the library

--- package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf test-reports/results.xml
-../CDE/cde xbuild ../mono-xunit-example/MonoXunit.sln
-../CDE/cde mono ../mono-xunit-example/packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe ../mono-xunit-example/bin/Debug/MonoXunit.Tests.dll /nunit testresults.xml
-
